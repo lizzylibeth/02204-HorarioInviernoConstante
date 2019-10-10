de4b3ed3df80787cfe6eddbe79ac755b52a3f39e
victo <victo@192.168.1.134>
2019-10-10 22:35:36 +0200

    :snowflake: Commit inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,41 @@
+/* 
+ * Copyright 2019 Elísabet Palomino .
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author Elísabet Palomino
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+        //Constante
+            final boolean HORARIO_INVIERNO = true;
+
+        //1º MANERA
+            System.out.println("- Horario de Invierno Nublado, frío y lluvioso: " + HORARIO_INVIERNO);      
+
+        //2º MANERA
+            System.out.printf("- El Horario de Invierno es nublado, frío y lluvioso: %b%n", HORARIO_INVIERNO);
+           
+             
+        
+        
+    }//main
+    
+}//class
